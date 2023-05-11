4e4a5dbc12f4bb79b2b18d157e11f16c67f59645
Artur Asfandiyarov <asf-artur@yandex.ru>
2023-05-11 23:25:45 +0500

    fixes 1

--- app/src/main/java/com/example/calmind/LoginFragment.kt
+++ app/src/main/java/com/example/calmind/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import com.example.calmind.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -19,15 +20,29 @@ class LoginFragment : Fragment() {
 
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonLogin.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_productsFragment)
+            /**
+             * TODO:
+             * здесь мы устанавливаем опции, чтобы форма авторизации не открывалась по нажатию на кнопку назад
+             *
+             */
+            val navOptions = navOptions {
+                this.popUpTo(R.id.nav_login){
+                    this.inclusive = true
+                }
+            }
+
+            /**
+             * TODO:
+             * здесь была проблема, т.к. были перепутаны ID по навигации, т.к. было несколько фрагментов с одинаковыми названиями
+             * раньше навигация бралась из файла @see [nav_graph.xml], а должна была из @see [mobile_navigation.xml]
+             */
+            findNavController().navigate(R.id.action_nav_login_to_nav_home, null, navOptions)
         }
     }
-
 }
